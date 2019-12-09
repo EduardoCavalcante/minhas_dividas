3914c9b547f48eacd8242c1502b7ec1a51271fa9
João Eduardo <joaoeduardodesousa@gmail.com>
2019-12-09 14:10:25 -0300

    config babel and webpack

---
+++ frontend/create_enviroment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yarn init -y
+yarn add @babel/core -D
+yarn add @babel/preset-env -D
+yarn add @babel/preset-react -D
+yarn add webpack -D
+yarn add webpack-cli -D
+yarn add react
+yarn add react-dom
+yarn add babel-loader -D
+yarn add webpack-dev-server -D
